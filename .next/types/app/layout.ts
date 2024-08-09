3c5f619637db4ae8c160790e818971eb006ff104
Jakehu2020 <71296004+Jakehu2020@users.noreply.github.com>
2024-08-09 14:08:37 -0500

    v3

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\khu\Desktop\Projects\Next\nextjs-dashboard\app\layout.tsx
+// File: C:\Users\khu\Desktop\Projects\nextjs-dashboard\nextjs-dashboard\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
